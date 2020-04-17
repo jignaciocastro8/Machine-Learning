880b5e6435b1dabf7c2cb83290cc2eb0b352a23e
jignaciocastro8 <jignaciocastro8@gmail.com>
2020-04-17 16:59:21 -0400

    Se dejó todo como estaba

--- tarea1.py
+++ tarea1.py
@@ -124,10 +124,3 @@ def plotModelo():
 #plotParam()
 #plotEcmVar()
 #plotModelo()
-
-"""Cambio para probar funcionamiento de las ramas en git"""
-"""Otro cambio para probar funcinamiento de las ramas en git"""
-"""Cambio test 1"""
-"""Cambio test 2"""
-
-
